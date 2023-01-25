806b261b0dd614940856c3e280fe63b8cd33b99c
Bonandi22 <elkerbonandi@gmail.com>
2023-01-25 18:11:12 +0000

    fisrt commit

---
+++ ModelOrder/Entiti/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace ModelOrder.Entiti
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product() { }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+
+    }
+}
